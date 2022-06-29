8225006509d23ce267de640fca7d7067ad9cdb83
olundel <o.shiryavceva@gmail.com>
2022-06-29 15:40:52 +0300

    add structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:meditation_app/internal/application.dart';
 
 import 'package:meditation_app/main.dart';
 
